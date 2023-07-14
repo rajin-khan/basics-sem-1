bdd47d397713f5f6d8f15887e30546a25011b061
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-14 16:44:08 +0600

    Update lameequation

--- Lab02/lameequation.c
+++ Lab02/lameequation.c
@@ -4,21 +4,19 @@
 #include<stdio.h>
 #include<math.h>
 
-int main()
-{
-    float a;
-    float b;
-    float c;
-    float x;
-    float ans;
+int main() {
+
+    float a, b, c, x, ans;
 
     printf("This program will calculate the value of 5x²-4x²+√(x)+3.\n");
+
     printf("Please enter a value of x:");
     scanf("%f", &x);
 
     a = 5*(pow(x,2));
     b = 4*(pow(x,2));
     c = sqrt(x);
+    
     ans = a-b+c+3;
 
     printf("The answer is = %.2f\n",ans);
