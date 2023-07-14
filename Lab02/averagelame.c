7530877c21c45f0ce212a4e2e4b0d1aa066a589d
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-14 14:10:12 +0600

    Update averagelame

--- Lab02/averagelame.c
+++ Lab02/averagelame.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
-int main()
-{
-    int num1;
-    int num2;
-    int num3;
-    int num4;
-    float average;
-    float sum;
+
+int main() {
+
+    int num1, num2, num3, num4;
+    float average, sum;
 
     printf("Please enter your first number:");
     scanf("%d", &num1);
+
     printf("Please enter your second number:");
     scanf("%d", &num2);
+
     printf("Please enter your third number:");
     scanf("%d", &num3);
+
     printf("Please enter your fourth number:");
     scanf("%d", &num4);
 
@@ -23,5 +23,4 @@ int main()
     printf("The Average is: %.2f\n", average);
     
     return 0;
-
 }
