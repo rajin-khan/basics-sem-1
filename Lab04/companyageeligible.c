c296533ac3d0c3e2995e821301ffd23d1fcd779b
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-18 02:29:23 +0600

    Update companyageeligible

--- Lab04/companyageeligible.c
+++ Lab04/companyageeligible.c
@@ -5,22 +5,22 @@ The criteria is that their age must be between 25 and 45.
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int age;
 
     printf("\nThis program will determine whether you are eligible to work or not.\n");
     printf("\nPlease enter your age:");
     scanf("%i", &age);
 
-    if ((age>=25)&&(age<=45))
-        {
-            printf("\nYou are of eligible age. Please proceed!\n\n");
-        }
-    else
-        {
-            printf("\nYou are too young, or too old.\n\n");
-        }
+    if ((age>=25)&&(age<=45)) {
+        
+        printf("\nYou are of eligible age. Please proceed!\n\n");
+    }
+    else {
+        
+        printf("\nYou are too young, or too old.\n\n");
+    }
 
     return 0;
 }
