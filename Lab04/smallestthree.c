4543c2217b87cf9ecef28c95b8b0b24321bce767
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-20 11:08:25 +0600

    Update smallestthree

--- Lab04/smallestthree.c
+++ Lab04/smallestthree.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int a, b, c;
 
     printf("\nThis program will read three numbers from you and print the smallest one from them.\n");
@@ -15,16 +15,16 @@ int main()
     printf("\nPlease enter the third number: ");
     scanf("%i", &c);
 
-    if (a<b && a<c)
-    {
+    if (a<b && a<c) {
+
         printf("\nThe smallest one is: %i\n\n", a);
     }
-    else if (b<c)
-        {
-            printf("\nThe smallest one is: %i\n\n", b);
-        }
-    else 
-    {
+    else if (b<c) {
+
+        printf("\nThe smallest one is: %i\n\n", b);
+    }
+    else  {
+        
         printf("\nThe smallest one is: %i\n\n", c);
     }
 
