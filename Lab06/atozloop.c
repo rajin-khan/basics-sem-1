d26a207622d806cdb05c1433db72a0aa51bc9431
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-22 11:33:02 +0600

    Update atozloop

--- Lab06/atozloop.c
+++ Lab06/atozloop.c
@@ -2,20 +2,20 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int let;
 
     printf("\nThe lowercase letters of the alphabet are:\n");
 
-    for (let='a'; let<='z'; let++) //iterates with the lower treashold being the ASCII value of a, and the upper one being the ASCII value of z.
-    {
-        if (let!='z') //makes sure that all letters are printed serially until z
-        {
+    for (let='a'; let<='z'; let++) { //iterates with the lower treashold being the ASCII value of a, and the upper one being the ASCII value of z.
+    
+        if (let!='z') { //makes sure that all letters are printed serially until z
+        
             printf("%c ", let);
         }
-        else
-        {
+        else {
+
             printf("%c \n", let); //here, for z, the code shifts and the next print statement goes to the new line, making it look better
         }
         
@@ -23,17 +23,16 @@ int main()
 
     printf("\nThe uppercase letters of the alphabet are:\n");
 
-    for (let='a'; let<='z'; let++)
-    {
-        if (let!='z')
-        {
+    for (let='a'; let<='z'; let++) {
+
+        if (let!='z') {
+            
             printf("%c ", let-32); //as the capital of the ASCII value of A is 32 less than a, and so on and so forth.
         }
-        else //the condition for making it print Z and ending the code on the next line
-        {
-            printf("%c \n\n", let-32); 
-        }
+        else { //the condition for making it print Z and ending the code on the next line
         
+            printf("%c \n\n", let-32); 
+        }    
     }
     
     return 0;
