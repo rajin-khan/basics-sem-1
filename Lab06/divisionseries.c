0501e768b9da65138487f1a896d5c44a1cd9dd91
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-22 11:35:32 +0600

    Update divisionseries

--- Lab06/divisionseries.c
+++ Lab06/divisionseries.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+    
     int x, n, sum=0; //x is a dummy character to run the program
 
     printf("\nThis program will compute the sum of the series 10000+2000+400+...+16.\n");
@@ -11,8 +11,8 @@ int main()
     printf("\nPlease enter any character to run the program.\n");
     scanf("%d", &x);
 
-    for (int i=10000; i>=8; i/=5) //The counter is divided by 5 every time, decreases till it is equal to or greater than 8
-    {
+    for (int i=10000; i>=8; i/=5) { //The counter is divided by 5 every time, decreases till it is equal to or greater than 8
+    
         printf("%d + ", i); 
         sum += i; 
     }
