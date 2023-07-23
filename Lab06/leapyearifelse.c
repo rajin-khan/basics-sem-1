cc50f089c259282098e90a84f8756f60d7d711ce
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-23 08:07:20 +0600

    Update leapyearifelse

--- Lab06/leapyearifelse.c
+++ Lab06/leapyearifelse.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int year;
 
     printf("\nThis program will check whether the year you enter is a leap year, or not.\n");
@@ -11,23 +11,23 @@ int main()
     printf("\nPlease enter a year: ");
     scanf("%d", &year);
 
-    if((year%100)==0) //this will check if it's a century year, e.g, 2000, 2100, 3400, 4000, etc
-    {
-        if ((year%400)==0) //check again if the century year is divisible by 400
-        {
+    if((year%100)==0) { //this will check if it's a century year, e.g, 2000, 2100, 3400, 4000, etc
+
+        if ((year%400)==0) { //check again if the century year is divisible by 400
+        
             printf("\n%d is a leap year.\n\n", year);
         }
-        else
-        {
+        else {
+
             printf("\n%d is not a leap year.\n\n", year);
         }
     }
-    else if ((year%4)==0) //check if the other years (apart from the century years) are divisible by 4 (the normal condition for a leap year).
-        {
+    else if ((year%4)==0) { //check if the other years (apart from the century years) are divisible by 4 (the normal condition for a leap year).
+        
             printf("\n%d is a leap year.\n\n", year);
         }
-        else
-        {
+        else {
+        
             printf("\n%d is not a leap year.\n\n", year);
         }
         
