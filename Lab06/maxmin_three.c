d2c86be0d01ace066bfa6d9dfa3cfb13b8e6126d
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-23 08:08:40 +0600

    Update maxmin_three

--- Lab06/maxmin_three.c
+++ Lab06/maxmin_three.c
@@ -5,8 +5,8 @@ and find the greatest and smallest of those three values.
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int a, b, c;
 
     printf("\nThis program will take three numbers from you and calculate the greatest and smallest from them.\n");
@@ -14,29 +14,28 @@ int main()
     printf("\nPlease enter three numbers:\n\n");
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a>b && a>c)
-    {
+    if (a>b && a>c) {
+
         printf("\nThe largest one is: %i\n", a);
     }
-    else if (b>c)
-        {
-            printf("\nThe largest one is: %i\n", b);
-        }
-    else 
-    {
+    else if (b>c) {
+        printf("\nThe largest one is: %i\n", b);
+    }
+    else {
+
         printf("\nThe largest one is: %i\n", c);
     }
 
-    if (a<b && a<c)
-    {
+    if (a<b && a<c) {
+
         printf("\nThe smallest one is: %i\n\n", a);
     }
-    else if (b<c)
-        {
-            printf("\nThe smallest one is: %i\n\n", b);
-        }
-    else 
-    {
+    else if (b<c) {
+
+        printf("\nThe smallest one is: %i\n\n", b);
+    }
+    else {
+        
         printf("\nThe smallest one is: %i\n\n", c);
     }
 
