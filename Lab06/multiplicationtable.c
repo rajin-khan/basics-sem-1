63175760484d34afeab30c384940e02aa9f054af
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-23 08:55:33 +0600

    Update multiplicationtable.c

--- Lab06/multiplicationtable.c
+++ Lab06/multiplicationtable.c
@@ -2,15 +2,15 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n, prod;
 
     printf("\nEnter number to print the multiplication table of: ");
     scanf("%d", &n);
 
-    for (int i=1; i<=10; i++)//Starts with the product with 1, and then continues till 10. Replace the 10 with any number to continue till that number.
-    {
+    for (int i=1; i<=10; i++) {
+        
         prod = n*i;
         printf("%d * %d = %d\n", n, i, prod); //Outputs x*y = ans, next lien prints x2*y = ans (one for every iteration)
     }
