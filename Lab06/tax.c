e725e76b1e1467c66c3a7b648476d5f937e65f3d
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-27 08:21:30 +0600

    Update tax

--- Lab06/tax.c
+++ Lab06/tax.c
@@ -13,8 +13,8 @@ Salary = 80,000 ~ 150,000 / Base Tax = 21,600.00, Excess = 33%
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     float sal, btx, tax, ttax; //variables for the salary, the base tax, the excess tax, and the total tax
 
     printf("\nThis program will calculate your tax after taking your salary as an input:\n");
@@ -22,30 +22,29 @@ int main()
     printf("\nPlease enter your salary: $");
     scanf("%f", &sal);
 
-    if ((sal>=0)&&(sal<15000))
-    {
+    if ((sal>=0)&&(sal<15000)) {
+
         btx = 0;
         tax = ((float)15/100)*(sal);
         ttax = btx + tax;
 
         printf("\nYour total tax for this month is = $%.2f\n\n", ttax);
     }
-    else if ((sal>=15000)&&(sal<30000))
-        {
-            btx = 2250;
-            tax = ((float)18/100)*(sal);
-            ttax = btx + tax;
+    else if ((sal>=15000)&&(sal<30000)) {
+        btx = 2250;
+        tax = ((float)18/100)*(sal);
+        ttax = btx + tax;
 
-            printf("\nYour total tax for this month is = $%.2f\n\n", ttax);
-        }
-    else if ((sal>=30000)&&(sal<50000))
-        {
-            btx = 5400;
-            tax = ((float)22/100)*(sal);
-            ttax = btx + tax;
+        printf("\nYour total tax for this month is = $%.2f\n\n", ttax);
+    }
+    else if ((sal>=30000)&&(sal<50000)) {
 
-            printf("\nYour total tax for this month is = $%.2f\n\n", ttax);
-        }
+        btx = 5400;
+        tax = ((float)22/100)*(sal);
+        ttax = btx + tax;
+
+        printf("\nYour total tax for this month is = $%.2f\n\n", ttax);
+    }
     else if ((sal>=50000)&&(sal<80000))
         {
             btx = 11000;
@@ -54,16 +53,16 @@ int main()
 
             printf("\nYour total tax for this month is = $%.2f\n\n", ttax);
         }
-    else if ((sal>=80000)&&(sal<150000))
-        {
-            btx = 21600;
-            tax = ((float)33/100)*(sal);
-            ttax = btx + tax;
+    else if ((sal>=80000)&&(sal<150000)) {
 
-            printf("\nYour total tax for this month is = $%.2f\n\n", ttax);
-        }
-    else 
-    {
+        btx = 21600;
+        tax = ((float)33/100)*(sal);
+        ttax = btx + tax;
+
+        printf("\nYour total tax for this month is = $%.2f\n\n", ttax);
+    }
+    else {
+        
         printf("\nYour salary exceeds the calculation bracket.\n\n");
     }
 
