7a976b9d2464e397614733ce85380f70a3d46de5
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-27 08:14:42 +0600

    Update quadratic_equation

--- Lab06/quadratic_equation.c
+++ Lab06/quadratic_equation.c
@@ -6,15 +6,13 @@ Inputs of a, b, and c will be taken from the user.
 #include<stdio.h>
 #include<math.h>
 
-int main()
-{
-    int a;
-    int b;
-    int c;
-    float det; //The determinant
-    float ans1, ans2; //The roots
+int main() {
+
+    int a, b, c;
+    float det, ans1, ans2; //The roots
 
     printf("\nThis program will calculate the roots of the equation ax²+bx+c=0.\n");
+
     printf("\nPlease enter the value of a:");
     scanf("%i",&a);
     printf("\nPlease enter the value of b:");
@@ -25,25 +23,25 @@ int main()
     int b² = b*b;
     det = (b²-(4*a*c));
 
-    if (det==0)
-    {
+    if (det==0) {
+
         printf("\nFor %dx²+%dx+%d,\n", a, b, c); //Visualising the equation makes it better for the user.
 
         ans1 = ans2 = ((-b)/(2*a));
          printf("\nThe roots are = %.2f, %.2f", ans1, ans2);
          printf("\nThe roots are real and equal.\n\n");
     }
-    else if (det>0)
-        {
-            printf("\nFor %dx²+%dx+%d,\n", a, b, c);
+    else if (det>0) {
 
-            ans1 = (-(b)+sqrt(b²-(4*a*c)))/((float)2*a);
-            ans2 = (-(b)-sqrt(b²-(4*a*c)))/((float)2*a);
-             printf("\nThe roots are = %.2f, %.2f", ans1, ans2);
-             printf("\nThe roots are real and distinct.\n\n");
-        }
-    else
-    {
+        printf("\nFor %dx²+%dx+%d,\n", a, b, c);
+
+        ans1 = (-(b)+sqrt(b²-(4*a*c)))/((float)2*a);
+        ans2 = (-(b)-sqrt(b²-(4*a*c)))/((float)2*a);
+        printf("\nThe roots are = %.2f, %.2f", ans1, ans2);
+        printf("\nThe roots are real and distinct.\n\n");
+    }
+    else {
+        
         printf("\nFor %dx²+%dx+%d,\n", a, b, c);
         printf("\nThe roots are not real (complex/imaginary).\n\n");
     }
