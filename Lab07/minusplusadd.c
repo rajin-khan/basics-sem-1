0dd106dcf75a53ef3492393d9b6fdfe003a26fca
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-29 22:06:00 +0600

    Update minusplusadd

--- Lab07/minusplusadd.c
+++ Lab07/minusplusadd.c
@@ -2,8 +2,7 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
     int sum, n, x;
 
     printf("\nThis program will compute the sum of the series 5-7+9-11...+n.\n");
@@ -12,23 +11,25 @@ int main()
 
     sum = 5;
     x=1;
+
     printf("%d - ", sum);
 
-    for (int i=7; i<=n; i+=2)
-    {    
-        if ((x%2)!=0)
-        {
+    for (int i=7; i<=n; i+=2) {
+
+        if ((x%2)!=0) {
+
             printf("%d + ", i);
             sum -= i;
             x++;
         }
-        else
-        {
+        else {
+
             printf("%d - ", i);
             sum +=i;
             x++;
         }       
     }
+    
     printf("\b\b= %d\n\n", sum);
 
     return 0;
