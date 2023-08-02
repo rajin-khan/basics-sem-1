1cb2108db2238a020020a0a2325798e82c11e650
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-02 09:58:25 +0600

    Update primereverse(improve_this)

--- Lab07/primereverse(improve_this).c
+++ Lab07/primereverse(improve_this).c
@@ -6,8 +6,8 @@ It will further output whether or not the reverse number is prime.
 #include<stdio.h>
 #include<math.h>
 
-int main()
-{
+int main() {
+
     int num, numd, rnum=0, dig=0, isprime=69; //'Flag'/Indicator for the prime number; can be set to any non-zero value, which means true
 
     printf("\nThis program will reverse your number and check if it is prime.\n\n");
@@ -18,14 +18,14 @@ int main()
     numd = num;
 
     int ndig = 0;
-    while (numd!=0)
-    {
+    while (numd!=0) {
+
         numd /=10;
         ndig++;
     }
 
-    for (; ndig>=0; ndig--)
-    {
+    for (; ndig>=0; ndig--) {
+
         dig = num%10;
         rnum += (dig*(pow(10, ndig-1)));
         num /= 10;
@@ -33,21 +33,21 @@ int main()
 
     printf("\nThe reverse of your number is: %d\n", rnum);
 
-    for (int i=2; i<=(rnum/2); i++)
-    {
-        if ((rnum%i)==0)
-        {
+    for (int i=2; i<=(rnum/2); i++) {
+
+        if ((rnum%i)==0) {
+
             isprime = 0;
             break;
         }
     }
 
-    if (isprime==0)
-    {
+    if (isprime==0) {
+
         printf("It is not a prime number.\n\n");
     }
-    else
-    {
+    else {
+        
         printf("It is a prime number.\n\n");
     }
 
