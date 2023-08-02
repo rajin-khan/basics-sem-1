36cf04e3759af2e4c098e6d28a15ced24b7afa37
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-02 10:00:40 +0600

    Update seriessum

--- Lab07/seriessum.c
+++ Lab07/seriessum.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int sum, n;
 
     printf("\nThis program will compute the sum of the series 3+6+12+...+n.\n");
@@ -12,8 +12,8 @@ int main()
 
     sum = 0;
 
-    for (int i=3; i<=n; i*=2)
-    {
+    for (int i=3; i<=n; i*=2) {
+        
         printf("%d + ", i);
         sum +=i;
     }
