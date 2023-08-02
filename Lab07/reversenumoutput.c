dd81153abefd51b899a8ca2004150db578f51395
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-02 09:59:27 +0600

    Update reversenumoutput

--- Lab07/reversenumoutput.c
+++ Lab07/reversenumoutput.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int num, dig; //Variables for the input number and the reversed digits
 
     printf("\nThis program will reverse your number.\n\n");
@@ -11,19 +11,19 @@ int main()
     printf("Please enter a number (an integer): ");
     scanf("%d", &num);
 
-    if (!(num<=0))
-    {
+    if (!(num<=0)) {
+
         printf("\nThe reversed number is: ");
-        while (num!=0)//Continues till number is decreases to 0.
-        {
+        while (num!=0) { //Continues till number is decreases to 0.
+        
             dig = num%10;
             printf("%d", dig); //Prints each of the last digits serially, hence showing the 'reversed' number
             num /= 10; //Last digit dropped
         }
         printf(" \n\n");
     }
-    else
-    {
+    else {
+        
         printf("\nPlease enter a positive number.\n\n");
     }
 
