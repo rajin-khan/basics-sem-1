a0787f0e7a59b0c4ee3fc3d5d757b13f2195f198
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-02 09:56:21 +0600

    Update perfectnum

--- Lab07/perfectnum.c
+++ Lab07/perfectnum.c
@@ -6,8 +6,8 @@ e.g. 6 is a perfect number; because proper factors of 6 are 1, 2, 3 and 1+2+3 = 
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int num, sum=0;
 
     printf("\nThis program will will check if your input number is a perfect number.\n\n");
@@ -17,10 +17,10 @@ int main()
 
     printf("\nThe factors are: ");
 
-    for (int i=1; i<=(num/2); i++) //Calculates and prints the factors, loop continues till num/2 as it is the largest possible factor
-    {
-        if ((num%i)==0) //Checks divisibility
-        {
+    for (int i=1; i<=(num/2); i++) { //Calculates and prints the factors, loop continues till num/2 as it is the largest possible factor
+    
+        if ((num%i)==0) { //Checks divisibility
+        
             printf("%d, ", i);
         }
     }
@@ -28,23 +28,24 @@ int main()
 
     printf("As ");
 
-    for (int i=1; i<=(num/2); i++) //Calculates and prints the sum of the factors
-    {
-        if ((num%i)==0)
-        {
+    for (int i=1; i<=(num/2); i++) { //Calculates and prints the sum of the factors
+    
+        if ((num%i)==0) {
+
             printf("%d + ", i);
             sum +=i;
         }
     }
     printf("\b\b = %d,\n", sum); //wowmoreformatting
 
-    if (sum==num)
-        {
-            printf("%d is a perfect number.\n\n", num);
-        }
-        else
-        {
-            printf ("%d is not a perfect number.\n\n", num);
-        }
+    if (sum==num) {
+
+        printf("%d is a perfect number.\n\n", num);
+    }
+    else {
+
+        printf ("%d is not a perfect number.\n\n", num);
+    }
+    
     return 0;
 }
