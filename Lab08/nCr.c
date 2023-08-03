ccd36064943a306bd736f77aba5bed4130ed0dec
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-03 07:43:20 +0600

    Update nCr

--- Lab08/nCr.c
+++ Lab08/nCr.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n, r, nr, ans; //variables for storing values of n, r, and n-r
     int rfact=1, nfact=1, nrfact=1; //the variables for r factorial, n factorial, and (n-r) factorial
 
@@ -16,33 +16,33 @@ int main()
 
     nr = n-r;
 
-    for (int i=1; i<=n; i++) //i iterates till it is less than n
-    {
+    for (int i=1; i<=n; i++) { //i iterates till it is less than n
+    
         nfact *= i; //the product of 1*2*3*...*n, continues till it reaches n, increases by 1 for every iteration
     }
 
-    for (int i=1; i<=nr; i++) //same as the last one, calculates (n-r) factorial
-    {
+    for (int i=1; i<=nr; i++) { //same as the last one, calculates (n-r) factorial
+    
         nrfact *= i;
     }
 
-    for (int i=1; i<=r; i++) //r factorial
-    {
+    for (int i=1; i<=r; i++) { //r factorial
+    
         rfact *= i;
     }
 
     ans = nfact/(rfact*nrfact);
 
-    if (r>n)
-    {
+    if (r>n) {
+
         printf("\nMath Error (r cannot be > n)\n\n");
     }
-    else if (r<0)
-    {
+    else if (r<0) {
+
         printf("\nMath Error (r must be greater than or equal to 0)\n\n");
     }
-    else
-    {
+    else {
+        
         printf("\n%dC%d = %d\n\n", n, r, ans);
     }
 
