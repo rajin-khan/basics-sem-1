f29d1bf1b449687191e73c0ef3ac47f11ce59db4
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-03 07:35:58 +0600

    Update binarytodecimak

--- Lab08/binarytodecimal.c
+++ Lab08/binarytodecimal.c
@@ -3,8 +3,8 @@
 #include<stdio.h>
 #include<math.h>
 
-int main()
-{
+int main() {
+
     int bi, xbi, dbi, obi, dec=0;
 
     printf("\nThis program will convert your binary number to its decimal equivalent.\n\n");
@@ -18,23 +18,23 @@ int main()
     xbi = bi;
 
     int ndig = 0;
-    while (dbi!=0)
-    {
+    while (dbi!=0) {
+
         dbi /=10;
         ndig++;
     }
 
     xbi = bi % 10;
 
-    for (int i=0; i<=ndig; i++)
-    {
-        if (xbi==0)
-        {
+    for (int i=0; i<=ndig; i++) {
+
+        if (xbi==0) {
+
             bi/=10;
             xbi = bi % 10;
         }
-        else
-        {
+        else {
+            
             dec += pow(2, i);
             bi/=10;
             xbi = bi % 10;
