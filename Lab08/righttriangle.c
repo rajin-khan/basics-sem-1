5a23d724b96fe7021c1f6b9d1ec58255f6c0ddaf
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-04 15:29:06 +0600

    Update righttriangle

--- Lab08/righttriangle.c
+++ Lab08/righttriangle.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int l;
 
     printf("\nThis program will print an asterisk right-triangle for the number of lines you enter.\n\n");
@@ -13,10 +13,10 @@ int main()
 
     printf("\n");
 
-    for (int i=1; i<=l; i++) //Loops and prints the number of lines till the input l
-    {
-        for (int a=1; a<=i; a++) //for each iteration, prints an asterisk for the current value of i (so first iteration has one *, second iteration, i.e, second line, has two *-s, and so on)
-        {
+    for (int i=1; i<=l; i++) { //Loops and prints the number of lines till the input l
+    
+        for (int a=1; a<=i; a++) { //for each iteration, prints an asterisk for the current value of i (so first iteration has one *, second iteration, i.e, second line, has two *-s, and so on)
+        
             printf("*");
         }
 
