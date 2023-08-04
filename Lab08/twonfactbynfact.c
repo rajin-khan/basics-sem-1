dc806d17526e09e6e315db005c148f5cf2559445
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-04 15:33:01 +0600

    Update twonfactbynfact

--- Lab08/twonfactbynfact.c
+++ Lab08/twonfactbynfact.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n, tn, ans; //variables for storing values of n, 2n, and the answer
     int nfact=1, tnfact=1; //the variables for n factorial, and 2n factorial are initialized
 
@@ -14,24 +14,24 @@ int main()
 
     tn = 2*n;
 
-    for (int i=1; i<=n; i++) //i iterates till it is less than n
-    {
+    for (int i=1; i<=n; i++) { //i iterates till it is less than n
+    
         nfact *= i; //the product of 1*2*3*...*n, continues till it reaches n, increases by 1 for every iteration
     }
 
-    for (int i=1; i<=tn; i++) //same as the last one, calculates 2n factorial
-    {
+    for (int i=1; i<=tn; i++) { //same as the last one, calculates 2n factorial
+    
         tnfact *= i;
     }
 
     ans = tnfact/nfact;
 
-    if (n==0)
-    {
+    if (n==0) {
+
         printf("\nCannot be computed as n must be > 0.\n\n");
     }
-    else
-    {
+    else {
+        
         printf("\n(2n)!/n! = %d\n\n", ans);
     }
     return 0;
