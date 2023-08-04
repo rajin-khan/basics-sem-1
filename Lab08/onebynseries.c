36271c4f9a1ab98b61fea7113ba0760d09e71368
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-04 15:25:56 +0600

    Update onebynseries

--- Lab08/onebynseries.c
+++ Lab08/onebynseries.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     float sum, n;
 
     printf("\nThis program will compute the sum of the series (1/1)+(1/2)+(1/3)...+(1/n).\n");
@@ -10,8 +10,8 @@ int main()
 
     sum = 0;
 
-    for (float i=1; i<=n; i++)
-    {
+    for (float i=1; i<=n; i++) {
+        
         sum += (1/i);
         printf("(1/%.f) + ", i);
     }
