b70a76c1173d086b178be1de547fb3ea2c03b836
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-06 14:32:16 +0600

    Update hollowparallelogram

--- Lab09/hollowparallelogram_mbyn.c
+++ Lab09/hollowparallelogram_mbyn.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int m, n;
 
     printf("\nThis program will print a hollow parallelogram of size m*n.\n\n");
@@ -16,23 +16,24 @@ int main()
 
     printf("\n");
 
-    for (int i=1; i<=n; i++)
-    {
-        for (int j=1; j<=n-i; j++) //the loop for printing the leading spaces
-        {
+    for (int i=1; i<=n; i++) {
+
+        for (int j=1; j<=n-i; j++) { //the loop for printing the leading spaces
+        
             printf(" ");
         }
-        for (int k=1; k<=m; k++)
-        {
-        if ((i==1)||(i==n)||(k==1)||(k==m)) //This condition checks if it is the first column or last, or the first row and last, since that is all the places it needs to print the asterisks
-            {
+        for (int k=1; k<=m; k++) {
+
+            if ((i==1)||(i==n)||(k==1)||(k==m)) { //This condition checks if it is the first column or last, or the first row and last, since that is all the places it needs to print the asterisks
+
                 printf("*");
             }
-            else
-            {
+            else {
+
                 printf(" "); //otherwise, a space is printed
             }
         }
+        
         printf("\n");
     }
 
