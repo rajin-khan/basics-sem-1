e0a4c5fad4537c9e627ca65d36977d568a3901a8
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-06 14:30:54 +0600

    Update hollowboxnlines

--- Lab09/hollowboxnlines.c
+++ Lab09/hollowboxnlines.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int m, n;
 
     printf("\nThis program will print a hollow box of n number of lines.\n\n");
@@ -13,16 +13,16 @@ int main()
 
     printf("\n");
 
-    for (int i=1; i<=n; i++)
-    {
-        for (int j=1; j<=n; j++)
-        {
-            if ((i==1)||(i==n)||(j==1)||(j==n))
-            {
-            printf("*");
+    for (int i=1; i<=n; i++) {
+
+        for (int j=1; j<=n; j++) {
+
+            if ((i==1)||(i==n)||(j==1)||(j==n)) {
+
+                printf("*");
             }
-            else
-            {
+            else {
+                
                 printf(" ");
             }
         }
