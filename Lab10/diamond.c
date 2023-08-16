6b58c2ea7e565001e797e011d4fc8635501d5998
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-16 08:01:16 +0600

    Update diamond

--- Lab10/diamond.c
+++ Lab10/diamond.c
@@ -16,8 +16,8 @@ for n number of lines
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n;
 
     printf("\nThis program will print a diamond for the number of lines you enter.\n");
@@ -27,27 +27,26 @@ int main()
 
     printf("\n");
 
-    for (int i=1; i<=n; i++)
-    {
-        for (int s=1; s<=n-i; s++)
-        {
+    for (int i=1; i<=n; i++) {
+
+        for (int s=1; s<=n-i; s++) {
+
             printf(" ");
         }
-        for (int j=1; j<=(2*i)-1; j++)
-        {
+        for (int j=1; j<=(2*i)-1; j++) {
             printf("*");
         }
         printf("\n");
     }
 
-    for (int i=n-1; i>=1; i--)
-    {
-        for (int s=1; s<=n-i; s++)
-        {
+    for (int i=n-1; i>=1; i--) {
+
+        for (int s=1; s<=n-i; s++) {
+
             printf(" ");
         }
-        for (int j=1; j<=(2*i)-1; j++)
-        {
+        for (int j=1; j<=(2*i)-1; j++) {
+            
             printf("*");
         }
         printf("\n");
