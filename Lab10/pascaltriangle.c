66e3b27bc50ddf094406eeb590b757c0f6528493
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-21 09:29:56 +0600

    Update pascaltriangle

--- Lab10/pascaltriangle.c
+++ Lab10/pascaltriangle.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n;
 
     printf("\nThis program will print pascal's triangle for the number of lines you enter.\n\n");
@@ -13,18 +13,18 @@ int main()
 
     printf("\n");
 
-    for (int i=1; i<=n; i++) //loop for printing the number of lines
-    {
-        for (int s=1; s<=n-i; s++) //loop for spaces
-        {
+    for (int i=1; i<=n; i++) { //loop for printing the number of lines
+    
+        for (int s=1; s<=n-i; s++) { //loop for spaces
+        
             printf(" ");
         }
-        for (int k=1; k <=i; k++)  //first half of pyramid
-        {  
+        for (int k=1; k <=i; k++) { //first half of pyramid
+          
             printf ("%d", k);
         }
-        for (int j=i-1; j>=1; j--)  //second half of pyramid, doesn't print the first line, starts from the second as when i=1, i-1 = 0
-        {  
+        for (int j=i-1; j>=1; j--) { //second half of pyramid, doesn't print the first line, starts from the second as when i=1, i-1 = 0
+          
             printf("%d", j);
         }
         printf("\n");
