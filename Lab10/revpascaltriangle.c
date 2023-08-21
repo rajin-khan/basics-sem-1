5f64d46878a65a7a3067de7f7acf9d1d733c6262
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-21 09:38:50 +0600

    Update revpascaltriangle

--- Lab10/revpascaltriangle.c
+++ Lab10/revpascaltriangle.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n;
 
     printf("\nThis program will print an inverted pascal's triangle for the number of lines you enter.\n\n");
@@ -13,18 +13,18 @@ int main()
 
     printf("\n");
 
-    for (int i=n; i>=1; i--) //loop for printing the number of lines
-    {
-        for (int s=1; s<=n-i; s++) //loop for spaces
-        {
+    for (int i=n; i>=1; i--) { //loop for printing the number of lines
+    
+        for (int s=1; s<=n-i; s++) { //loop for spaces
+        
             printf(" ");
         }
-        for (int k=1; k <=i; k++)  //first half of pyramid (right triangle)
-        {  
+        for (int k=1; k <=i; k++) { //first half of pyramid (right triangle)
+         
             printf ("%d", k);
         }
-        for (int j=i-1; j>=1; j--)  //second half of pyramid, doesn't print the first line of the triangle
-        {  
+        for (int j=i-1; j>=1; j--) { //second half of pyramid, doesn't print the first line of the triangle
+         
             printf("%d", j);
         }
         printf("\n");
