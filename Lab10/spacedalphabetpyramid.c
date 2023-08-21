7f9254553030d9d0620691aa8bdaa8f76b7794a0
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-21 09:40:42 +0600

    Update spacedalphabetpyramid

--- Lab10/spacedalphabetpyramid.c
+++ Lab10/spacedalphabetpyramid.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n;
 
     printf("\nThis program will print a spaced alphabet pyramid for the number of lines you enter.\n\n");
@@ -13,14 +13,14 @@ int main()
 
     printf("\n");
 
-    for (int i=1; i<=n; i++) 
-    {
-        for (int s=1; s<=n-i; s++)
-        {
+    for (int i=1; i<=n; i++)  {
+
+        for (int s=1; s<=n-i; s++) {
+
             printf(" ");
         }
-        for (int j=1; j<=i; j++)
-        {
+        for (int j=1; j<=i; j++) {
+
             printf("%c ", 'A'+(j-1));
         }
         printf("\n");
