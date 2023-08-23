05454bde8fee8373f4697392b49eeb88aefc2002
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-23 15:48:14 +0600

    Update nth_perfectnum

--- Lab11/nth_perfectnum.c
+++ Lab11/nth_perfectnum.c
@@ -2,8 +2,8 @@
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n;
 
     printf("\nThis program will print the n-th perfect number.\n\n");
@@ -16,26 +16,28 @@ int main()
     int count=0;
     int i=1;
 
-    while (count<n)
-    {
+    while (count<n) {
+
         int sum = 0;
 
-        for (int k=1; k<=(i/2); k++)
-        {
-            if (i%k==0)
-            {
+        for (int k=1; k<=(i/2); k++) {
+
+            if (i%k==0) {
+
                 sum += k;
             }
         }
-        if (i==sum)
-        {
+
+        if (i==sum) {
+
             count++;
 
-            if (count==n)
-            {
-            printf("%d", i);
+            if (count==n) {
+
+                printf("%d", i);
             }
         }
+        
         i++;
     }
 
