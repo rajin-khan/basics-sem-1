0a0affc237b949e8807205a2934cea4a2aa45b9a
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-23 15:43:40 +0600

    Update spacedinvertedpyramid

--- Lab10/spacedinvertedpyramic.c
+++ Lab10/spacedinvertedpyramic.c
@@ -16,8 +16,8 @@ for n number of lines
 
 #include<stdio.h>
 
-int main()
-{
+int main() {
+
     int n;
 
     printf("\nThis program will print a spaced asterisk pyramid.\n");
@@ -25,16 +25,17 @@ int main()
     printf("\nPlease enter the number of lines: ");
     scanf("%d", &n);
 
-    for (int i=n; i>=1; i--)
-    {
-        for (int s=1; s<=n-i; s++)
-        {
+    for (int i=n; i>=1; i--) {
+
+        for (int s=1; s<=n-i; s++) {
+
             printf(" ");
         }
-        for (int j=1; j<=(2*i)-1; j++)
-        {
+        for (int j=1; j<=(2*i)-1; j++) {
+
             printf("*");
         }
+        
         printf("\n\n");
     }
 
