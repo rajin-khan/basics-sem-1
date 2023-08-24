f0f0aac07b3800aa6a693d1e5178bc81398615d4
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-24 11:08:13 +0600

    Update summation_userfunction

--- Lab11/summation_userfunction.c
+++ Lab11/summation_userfunction.c
@@ -4,8 +4,8 @@
 
 int summation(int x); //Since the print fucntion I defined is at the bottom of the code, and most general compilers read top to bottom, I had to include the header of my function before the body of the main function
 
-int main()
-{
+int main() {
+
     int n, ans; //here, n is the local variable of the main function 
 
     printf("\nThis program will calculate the summation up to the number you enter.\n");
@@ -20,15 +20,15 @@ int main()
     return 0;
 }
 //   the name  arg <- here, any value can be used other than x.
-int summation(int x) //This is where the user defined function begins, int x is the argument of the function, and is replaced with whatever number (in this case, n) that I "call" this function with.
-{
+int summation(int x) { //This is where the user defined function begins, int x is the argument of the function, and is replaced with whatever number (in this case, n) that I "call" this function with.
+
     //In this function, x is the local variable of the function summation
     int sum=0; //Since the function was defined to be an integer value, it will return an integer value, and so the value (stored in the variable) that will be returned must be declared as the same type (in this case, int).
 
     printf("\n"); //Just exclude all the print statements if you don't wanna be fancy about it
 
-    for (int i=1; i<=x; i++)
-    {
+    for (int i=1; i<=x; i++) {
+        
         printf("%d + ", i);
         sum += i;
     }
